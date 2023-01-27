2bd4dc6bdd55c732113199ffff6c31db5aa74a54
Victoria <vikochka90@mail.ru>
2023-01-27 12:43:21 +0300

    new mwthod

---
+++ ArraySorted/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+string[] ArraySort(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] sortedArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            sortedArray[j] = array[i];
+            j++;
+        }
+    }
+    return sortedArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($" '{array[i]}' , ");
+    }
+    Console.Write($" '{array[array.Length - 1]}'");
+}
+
+string[] givenArray = { "hello", "2", "world", ";-)", "1234", "123", "-2" };
+PrintArray(ArraySort(givenArray));
